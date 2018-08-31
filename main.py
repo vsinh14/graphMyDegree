f569ce263ecc86b66c2abaff3d30a04d5ef233ee
Kenton Lam <kenton_lam@outlook.com>
2018-08-31 21:42:26 +1000

    Move to main folder.

---
+++ main.py
@@ -0,0 +1,6 @@
+from uq_data.scraper import *
+
+if __name__ == '__main__':
+    parse_course_code('MATH1051')
+    raise 'a'
+    parse_major_url('https://my.uq.edu.au/programs-courses/plan_display.html?acad_plan=SOFTWX2342')
