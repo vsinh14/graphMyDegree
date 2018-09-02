2574a3b77f5720883ad7ed0ccfd126ca987bd711
Kenton Lam <kenton_lam@outlook.com>
2018-09-02 13:08:12 +1000

    Add part letter to soft eng course data.

---
+++ major_name_to_int.py
@@ -0,0 +1,22 @@
+import json
+
+
+def main():
+    with open('data/SOFTWX2342/major_parts.json') as f:
+        major_parts = json.load(f)
+
+    with open('data/SOFTWX2342/course_data.json') as f:
+        course_data = json.load(f)
+
+    for course, parts in major_parts.items():
+        min_parts = min(x.split(' ')[1] for x in parts)
+        print(course, parts, min_parts)
+        course_data[course]['major_part'] = min_parts
+
+    with open('data/SOFTWX2342/course_data.json', 'w') as f:
+        json.dump(course_data, f, indent=4)
+
+
+
+if __name__ == '__main__':
+    main()
