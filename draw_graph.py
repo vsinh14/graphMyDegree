6760ed473dc5950c4d3bf8a847d16badd51dc230
Kenton Lam <kenton_lam@outlook.com>
2018-09-02 17:09:02 +1000

    Try/catch for parts.

--- draw_graph.py
+++ draw_graph.py
@@ -25,7 +25,11 @@ def draw_graph(ds: Data_structure, file_name):
             top = '<HR/>' if semester & 1 else ''
             bottom = '<HR/>' if semester & 2 else '' 
             name = graphviz.nohtml(data[key].getName()).replace('&', '&amp;')
-            drawn.node(key, label=f'<<FONT FACE="Arial"><TABLE BORDER="0"><TR><TD></TD></TR>{top}<TR><TD>{key}</TD></TR>{bottom}<TR><TD><FONT POINT-SIZE="10">{name}</FONT></TD></TR></TABLE></FONT>>', style='filled', fillcolor = colorDict[col] )
+            try:
+                colour = colorDict[col]
+            except KeyError:
+                colour = 'white'
+            drawn.node(key, label=f'<<FONT FACE="Arial"><TABLE BORDER="0"><TR><TD></TD></TR>{top}<TR><TD>{key}</TD></TR>{bottom}<TR><TD><FONT POINT-SIZE="10">{name}</FONT></TD></TR></TABLE></FONT>>', style='filled', fillcolor = colour)
     for edge in graph.edges:
         drawn.edge(edge[0], edge[1])
     drawn.attr(label=r'\n\nOrange is part A electives\nGreen is part B electives\nYellow is part C electives\nBlue is part D electives')
