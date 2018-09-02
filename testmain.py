aac54d8d92d2df00b2341775ed0cda20e7a75756
Kenton Lam <kenton_lam@outlook.com>
2018-09-02 16:40:20 +1000

    Wrap testmain in function.

--- testmain.py
+++ testmain.py
@@ -12,76 +12,81 @@ graph = dataStructure.getGraph()
 data = dataStructure.getData()
 orCounter = 0
 
-def courseCheck(dataDict, string):
-    if string in dataDict:
-        return true
-    return false
+def generate_graph(major_code):
 
-def recursiveRelation(dataStruct, key, part):
-    if(isinstance(part, list)):
-        #only one subject
-        dataStruct.addPreRequisite(part[0], key)
-        return
-    #actual recursion
-    actualRecursion(dataStruct, key, part)
+    def courseCheck(dataDict, string):
+        if string in dataDict:
+            return true
+        return false
+
+    def recursiveRelation(dataStruct, key, part):
+        if(isinstance(part, list)):
+            #only one subject
+            dataStruct.addPreRequisite(part[0], key)
+            return
+        #actual recursion
+        actualRecursion(dataStruct, key, part)
 
 
-def actualRecursion(dataStruct, key, part):
-    #type all base case do nothing different
-    global orCounter 
-    if(part['type'] == "all"):
-        for i in part['children']:
-            if(isinstance(i, str)):
-                dataStruct.addPreRequisite(i, key)
-            else:
-                actualRecursion(dataStruct, key, i)
-    #type any or node before 
-    
-    if(part['type'] == "any"):
-        orName = "orNode" + str(orCounter)
-        orCounter+=1
-        dataStruct.addSubject(orName, Subject(orName))
-        for i in part["children"]:
-            if(isinstance(i, str)):
-                dataStruct.addPreRequisite(i, orName)
-            else:
-                actualRecursion(dataStruct, orName, i)
-        dataStruct.addPreRequisite(orName, key)
+    def actualRecursion(dataStruct, key, part):
+        #type all base case do nothing different
+        global orCounter 
+        if(part['type'] == "all"):
+            for i in part['children']:
+                if(isinstance(i, str)):
+                    dataStruct.addPreRequisite(i, key)
+                else:
+                    actualRecursion(dataStruct, key, i)
+        #type any or node before 
         
-#adding all subjects and information associated with subject
-with open("data/SOFTWX2342/course_data.json") as json_file:
-    data1= js.load(json_file)
-    for k, p in data1.items():
-        courseSubject = Subject(p)
-        courseSubject.addValues(p['course_name'], p['semesters'], p['major_part'] )
-        dataStructure.addSubject(k, courseSubject)
-# add prereqs
-with open("data/SOFTWX2342/prerequisites.json") as pre_file:
-    data2= js.load(pre_file)
-    for k, p in data2.items():
-        if(len(p) == 0):
-            continue
-        recursiveRelation(dataStructure, k, p)
+        if(part['type'] == "any"):
+            orName = "orNode" + str(orCounter)
+            orCounter+=1
+            dataStruct.addSubject(orName, Subject(orName))
+            for i in part["children"]:
+                if(isinstance(i, str)):
+                    dataStruct.addPreRequisite(i, orName)
+                else:
+                    actualRecursion(dataStruct, orName, i)
+            dataStruct.addPreRequisite(orName, key)
+            
+    #adding all subjects and information associated with subject
+    with open("data/MECHAX2342/course_data.json") as json_file:
+        data1= js.load(json_file)
+        for k, p in data1.items():
+            courseSubject = Subject(p)
+            courseSubject.addValues(p['course_name'], p['semesters'], p['major_part'] )
+            dataStructure.addSubject(k, courseSubject)
+    # add prereqs
+    with open("data/MECHAX2342/prerequisites.json") as pre_file:
+        data2= js.load(pre_file)
+        for k, p in data2.items():
+            if(len(p) == 0):
+                continue
+            recursiveRelation(dataStructure, k, p)
 
-graph = dataStructure.getGraph()
-
-
-edited = True
-while edited:
-    edited = False 
-    for i, node in enumerate(dict(graph.nodes())):
-        if node.startswith('or'):
-            if graph.in_degree(node) == 0 or graph.out_degree(node) == 0:
-                graph.remove_node(node)
-                del dataStructure.data[node]
-                edited = True
-            elif graph.in_degree(node) == 1:
-                single_parent = list(graph[node].keys())[0]
-                for child in graph.predecessors(node):
-                    dataStructure.addPreRequisite(child, single_parent)
-                graph.remove_node(node)
-                del dataStructure.data[node]
-                edited = True
+    graph = dataStructure.getGraph()
 
 
-draw_graph(dataStructure)
+    edited = True
+    while edited:
+        edited = False 
+        for i, node in enumerate(dict(graph.nodes())):
+            if node.startswith('or'):
+                if graph.in_degree(node) == 0 or graph.out_degree(node) == 0:
+                    graph.remove_node(node)
+                    del dataStructure.data[node]
+                    edited = True
+                elif graph.in_degree(node) == 1:
+                    single_parent = list(graph[node].keys())[0]
+                    for child in graph.predecessors(node):
+                        dataStructure.addPreRequisite(child, single_parent)
+                    graph.remove_node(node)
+                    del dataStructure.data[node]
+                    edited = True
+
+
+    draw_graph(dataStructure)
+
+if __name__ == '__main__':
+    generate_graph('SOFTWX2342')
